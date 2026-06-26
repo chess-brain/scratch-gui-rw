5638881a737579b4683b092b749a5e10cb4e5d44
ScratchCatStudios <romanarsenyk11@gmail.com>
2026-06-26 19:36:24 +0300

    new addon

---
+++ src/addons/addons/todo/_manifest_entry.js
@@ -0,0 +1,28 @@
+const manifest = {
+    "editorOnly": true,
+    "name": "To-Do",
+    "description": "Record your project to-do!",
+    "tags": ["new", "recommended","astraeditor"],
+    "credits": [
+        {
+            "name": "KOSHINO",
+            "link": "https://github.com/KOSHINOawa"
+        },
+        {
+            "name": "RyaninCn11",
+            "link": "https://github.com/RyaninCn11"
+        }
+    ],
+    "enabledByDefault": true,
+    "userscripts": [
+        {
+            "url": "userscript.js"
+        }
+    ],
+    "userstyles": [
+        {
+            "url": "userstyle.css"
+        }
+    ]
+};
+export default manifest;
